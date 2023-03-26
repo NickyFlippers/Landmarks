45d4976be1a6e7862e5b3b327d2b65780ec07618
NickyFlippers <118223056+NickyFlippers@users.noreply.github.com>
2023-03-26 10:41:23 -0400

    Initial Commit

---
+++ LandmarkProject/LandmarkProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkProjectApp.swift
+//  LandmarkProject
+//
+//  Created by Nicholas Gramenos on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
