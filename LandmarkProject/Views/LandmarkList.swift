1fb6150595b70956390e4661ee82062da4cde814
NickyFlippers <118223056+NickyFlippers@users.noreply.github.com>
2023-03-29 14:44:26 -0400

    Added assets and created/combined views.

---
+++ LandmarkProject/Views/LandmarkList.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkList.swift
+//  LandmarkProject
+//
+//  Created by Nicholas Gramenos on 3/26/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
